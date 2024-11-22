841d7aabdf059b4daccd4015f9a5a80d80ae7970
Ethan Rowe <ethanrowe5379@gmail.com>
2024-11-22 13:35:17 +0000

    Basis for CV website, will be using base react, no framework to understand the basics

---
+++ src/Pages/Home/home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function Home(){
+    return(
+        <h1>In Development...</h1>
+    );
+}
+
---
+++ src/Pages/main.js
@@ -0,0 +1,15 @@
+
+export default function Main(){
+    return(
+        <header>
+            <h1>C:/user/ethan.rowe<span class="blink">_</span></h1>
+            <nav>
+                <a href="/home">Home</a>
+                <span> / </span>
+                <a href="../index">About</a>
+                <span> / </span>
+                <a href="../index">Projects</a>
+            </nav>
+        </header>
+    );
+}
